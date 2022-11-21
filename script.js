0b5ac39e8a44284a67a5b67e19b02d2aa42f185a
not-the <87151784+not-the@users.noreply.github.com>
2022-11-21 00:09:25 -0500

    Condensed code and added config options

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // DOM
-const container = document.getElementById('container');
+// const container = document.getElementById('container');
+const c_container = document.getElementById('c');
 const scoreP1 = document.getElementById('score_p1');
 const scoreP2 = document.getElementById('score_p2');
 const misc = document.getElementById('misc');
@@ -14,18 +15,18 @@ const center_y = height / 2 - size;
 size *= resize; // square = 32px
 const paddle_ratio = 3;
 const paddle_height = size * paddle_ratio;
-
-const ballSpeed = 8; // ball speed
-const speed = 6; // paddle speed
 const low_height = height - size*3;
 const boundary_left = size * 2;
 const boundary_right = (width - size * 2) - size;
 
-// Controls
-
+// Game
+var config = {
+    ball_speed: 8,
+    paddle_speed: 6,
+}
 var pressed = {};
 var velocity = {
-    x: ballSpeed,
+    x: config.ball_speed,
     y: 0,
 }
 var score = {
@@ -37,10 +38,9 @@ var game = {
     winstate: false,
 }
 
-
 // Canvas
 let app = new PIXI.Application({ width:width, height:height });
-container.appendChild(app.view);
+c_container.appendChild(app.view);
 
 /** Creates a sprite and returns it */
 function addSprite(sX=resize, sY=resize, posX=0, posY=0) {
@@ -55,11 +55,20 @@ function addSprite(sX=resize, sY=resize, posX=0, posY=0) {
 
 // Sprites
 let ball = addSprite(resize, resize, center_x, center_y);
-let player1 = addSprite(resize, resize * paddle_ratio, size, center_y - size);
-let player2 = addSprite(resize, resize * paddle_ratio, width - (size * 2), center_y - size);
+var player = {
+    1: addSprite(resize, resize * paddle_ratio, size, center_y - size),
+    2: addSprite(resize, resize * paddle_ratio, width - (size * 2), center_y - size),
+}
+player['1'].keyrise = 'w';
+player['1'].keyfall = 's';
+player['2'].keyrise = 'ArrowUp';
+player['2'].keyfall = 'ArrowDown';
 
+// Game
+
+/** Reset ball and display scores */
 function reset() {
-    velocity.x = ballSpeed;
+    velocity.x = config.ball_speed;
     velocity.y = 0;
     ball.position.x = center_x;
     ball.position.y = center_y;
@@ -73,49 +82,38 @@ let elapsed = 0.0;
 app.ticker.add((delta) => {
     elapsed += delta;
     if(game.paused) return;
+    const s = config.paddle_speed * delta;
 
-    const s = speed * delta;
-
-    if(pressed['w'] == true && player1.position.y - s >= 0) player1.position.y -= s;
-    if(pressed['s'] == true && player1.position.y + s <= low_height) player1.position.y += s;
-
-    if(pressed['ArrowUp'] == true && player2.position.y - s >= 0) player2.position.y -= s;
-    if(pressed['ArrowDown'] == true && player2.position.y + s <= low_height) player2.position.y += s;
+    /** Moves the player when button is pressed */
+    function moveCheck(p='1') {
+        let target = player[p];
+        if(pressed[target.keyrise] && target.position.y - s >= 0) target.position.y -= s;
+        if(pressed[target.keyfall] && target.position.y + s <= low_height) target.position.y += s;
+    }
+    moveCheck('1');
+    moveCheck('2');
     
-
-    // Left
-    if(ball.position.x <= boundary_left && !game.winstate) {
+    /** Detect collision */
+    function paddleCheck(p='1') {
+        if(p == '1' && ball.position.x > boundary_left  || game.winstate) return;
+        if(p == '2' && ball.position.x < boundary_right || game.winstate) return;
+    
         // Paddle aligned
-        if(ball.position.y + size >= player1.position.y && ball.position.y <= player1.position.y + paddle_height) {
-            console.log('bounce!');
+        if(ball.position.y + size >= player[p].position.y && ball.position.y <= player[p].position.y + paddle_height) {
+            // console.log('bounce!');
             velocity.x *= -1;
-            velocity.y = (ball.position.y - player1.position.y - 32) / 10;
+            velocity.y = (ball.position.y - player[p].position.y - 32) / 10;
         } else {
-            console.log('out of bounds, left');
+            // console.log('out of bounds');
             game.winstate = true;
-            score['2']++;
-            setTimeout(() => {
-                reset();
-            }, 1000);
+            let winner = p == '1' ? '2' : '1';
+            console.log(`Player ${winner} has scored`)
+            score[winner]++;
+            setTimeout(reset, 1000);
         }
     }
-
-    // Right
-    if(ball.position.x >= boundary_right && !game.winstate) {
-        // Paddle aligned
-        if(ball.position.y + size >= player2.position.y && ball.position.y <= player2.position.y + paddle_height) {
-            console.log('bounce!');
-            velocity.x *= -1;
-            velocity.y = (ball.position.y - player2.position.y - 32) / 10;
-        } else {
-            console.log('out of bounds, right');
-            game.winstate = true;
-            score['1']++;
-            setTimeout(() => {
-                reset();
-            }, 1000);
-        }
-    }
+    paddleCheck('1');
+    paddleCheck('2');
 
     // Vertical bounce
     if(ball.position.y <= 0 || ball.position.y >= low_height + 64) {
@@ -123,6 +121,7 @@ app.ticker.add((delta) => {
         velocity.y *= -1;
     }
 
+    // Move ball
     ball.x += velocity.x * delta;
     ball.y += velocity.y * delta;
 });
@@ -134,7 +133,16 @@ document.addEventListener('keydown', event => {
 document.addEventListener('keyup', event => {
     if(event.key == 'Escape') {
         game.paused = !game.paused;
+        misc.innerText = 'Paused';
         misc.style.visibility = game.paused ? 'visible' : 'hidden';
     }
     pressed[event.key] = false;
 });
+
+document.querySelectorAll('.config').forEach(element => element.addEventListener('change', event => {
+    // if(event.key != 'Enter') return;
+    let name = element.id;
+    let value = element.value;
+    config[name] = value;
+    reset();
+}));
